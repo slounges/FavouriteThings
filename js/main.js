b095c6075386e0e59768bf1e7f0de15004c9f7b3
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-11-15 04:15:39 -0500

    data showing

--- js/main.js
+++ js/main.js
@@ -1,57 +1,28 @@
-// import your packages here
 import { fetchData } from "./modules/DataMiner.js";
 
 (() => {
-    // stub * just a place for non-component-specific stuff
-    console.log('loaded');
-    
-    function popErrorBox(message) {
-        alert("Something has gone horribly, horribly wrong");
-    }
-
+    // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     function handleDataSet(data) {
         let userSection = document.querySelector('.user-section'),
             userTemplate = document.querySelector('#user-template').content;
 
+        // loop through the JavaScript object and for each user, make a copy of the user template we find at the bottom of index.html, populate it with the user's data, and put that fresh copy in the users section in index.html
+
         for (let user in data) {
-            debugger;
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
 
-            currentUserText[1].src = `images/${data[user].Image}`;
-            currentUserText[2].textContent = data[user].Title;
-            currentUserText[3].textContent = data[user].Description;
+            currentUserText[1].src = `images/${data[user].image}`;
+            currentUserText[2].textContent = data[user].title;
+            currentUserText[3].textContent = data[user].description;
 
             // add this new user to the view
             userSection.appendChild(currentUser);
         }
+
+        console.log(data);
     }
 
-    function retrieveProjectInfo() {
-        // test for an ID
-        // check for an id, and if there isn't one, then don't try the fetch call
-        // because it'll break (the PHP will choke)
-        if (!event.target.id) { return }
+    fetchData("./includes/index.php").then(data => handleDataSet(data)).catch(err => { console.log(err); });
 
-        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
-    }
-
-    function renderPortfolioThumbnails(thumbs) {
-        let userSection = document.querySelector('.user-section'),
-            userTemplate = document.querySelector('#user-template').content;
-
-        for (let user of thumbs) {
-            let currentUser = userTemplate.cloneNode(true),
-                currentUserText = currentUser.querySelector('.user').children;
-
-            currentUserText[1].src = `images/${thumbs[user].avatar}`;
-            currentUserText[1].id = thumbs[user].id;
-            // add this new user to the view
-            userSection.appendChild(currentUser);
-        }
-
-        userSection.addEventListener("click", retrieveProjectInfo);
-    }
-        
-    fetchData("./includes/index.php").then(data => renderPortfolioThumbnails(data)).catch(err => console.log(err));
 })();
